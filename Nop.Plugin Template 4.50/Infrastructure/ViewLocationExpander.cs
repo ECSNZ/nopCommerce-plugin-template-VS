b4dc6790259d3e63284b1334a162501bbb67bfa1
Greg Eveleens <greg@ecsnz.com>
2023-12-18 14:40:30 +1300

    Add more paths as these are generally needed

--- Nop.Plugin Template 4.50/Infrastructure/ViewLocationExpander.cs
+++ Nop.Plugin Template 4.50/Infrastructure/ViewLocationExpander.cs
@@ -16,14 +16,30 @@ namespace $safeprojectname$.Infrastructure
         {
             if (context.AreaName == "Admin")
             {
+                // Admin paths
                 viewLocations = new[] {
-                    $"~/Plugins/{Defaults.SYSTEM_NAME}/Areas/Admin/Views/{{1}}/{{0}}.cshtml"
+                    $"~/Plugins/{Defaults.SYSTEM_NAME}/Areas/Admin/Views/{{1}}/{{0}}.cshtml",
+                    $"~/Plugins/{Defaults.SYSTEM_NAME}/Areas/Admin/Views/{{0}}.cshtml",
+                    $"~/Plugins/{Defaults.SYSTEM_NAME}/Areas/Admin/Views/Shared/{{1}}/{{0}}.cshtml",
+                    $"~/Plugins/{Defaults.SYSTEM_NAME}/Areas/Admin/Views/Shared/{{0}}.cshtml",
                 }
                 .Concat(viewLocations);
             }
             else
             {
-                viewLocations = new[] {
+                // Theme Paths
+                viewLocations = new string[]
+                {
+                    $"~/Plugins/{Defaults.SYSTEM_NAME}/Themes/Pacific/Views/{{1}}/{{0}}.cshtml",
+                    $"~/Plugins/{Defaults.SYSTEM_NAME}/Themes/Pacific/Views/{{0}}.cshtml",
+                    $"~/Plugins/{Defaults.SYSTEM_NAME}/Themes/Pacific/Views/Shared/{{1}}/{{0}}.cshtml",
+                    $"~/Plugins/{Defaults.SYSTEM_NAME}/Themes/Pacific/Views/Shared/{{0}}.cshtml",
+                }
+                .Concat(viewLocations);
+
+                // Plugin Paths
+                viewLocations = new[]
+                {
                     $"~/Plugins/{Defaults.SYSTEM_NAME}/Views/{{1}}/{{0}}.cshtml",
                     $"~/Plugins/{Defaults.SYSTEM_NAME}/Views/{{0}}.cshtml",
                     $"~/Plugins/{Defaults.SYSTEM_NAME}/Views/Shared/{{1}}/{{0}}.cshtml",
